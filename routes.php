d792fa19bcbabd68476dc5ca0b4a6f9e9e4002c8
David Alvarez <hola@alvarezdavid.com>
2021-04-10 22:27:25 -0600

    New Changes

--- routes.php
+++ routes.php
@@ -1,6 +1,31 @@
 <?php
     
-    Route::get('/',function() { echo 'Hello, World!'; });
-    
+    Route::get('/',function() {  view('libro'); });
+    Route::get('libros/',function() { view('libro');});
+    Route::get('libro1/',function() { view('libro1');});
+    Route::get('libro2',function() { view('libro2');});
+    Route::get('libro3',function() { view('libro3');});
+    Route::get('libro4',function() { view('libro4');});
+    Route::get('libros/(:number)', function($id) {
+      //  echo 'The id is: ' . $id;
+      
+        switch($id){
+            case 1:
+             view('libro1');
+                break;
+            case 2:
+                view('libro'.$id);
+                break;
+            case 3:
+                view('libro-'.$id);
+                break;
+            case 4:
+                view('libro-'.$id);
+                break;
+            default:
+                view('libro');
+                break;
+        }
+      });
     Route::dispatch();
 ?>
