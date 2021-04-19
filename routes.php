32e2eb7f90819e4af11012f8b2b91be8642e1074
David Alvarez <hola@alvarezdavid.com>
2021-04-18 21:20:52 -0600

    Framework changes

--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
 <?php
-    
-    Route::get('/',function() {  view('libro'); });
+
+  Route::resource('professor', 'ProfessorController');
+
+    Route::get('/',function() {  echo 'Hello World'; });
     Route::get('libros',function() { view('libro');});
     Route::get('libro1',function() { view('libro1');});
     Route::get('libro2',function() { view('libro2');});
