cbc2bd1a4fe04961daedd1c2fab25169aa233cdd
David Alvarez <hola@alvarezdavid.com>
2021-04-18 21:21:25 -0600

    Framework changes

---
+++ views/libro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="card">
+            <img src="https://media.wiley.com/product_data/coverImage300/55/11180937/1118093755.jpg" alt="Avatar" style="width:100%">
+            <div class="container">
+            <h3><b>Operating System Concepts</b></h3>
+            <p>Abraham Silberschatz</p>
+            <p>John Wiley & Sons</p>
+            <p>ENGLISH | 9th | 2012 | 976</p>
+            
+            </div>
+        </div> 
+    </div>
+</body>
+</html>
